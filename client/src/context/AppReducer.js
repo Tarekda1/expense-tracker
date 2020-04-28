dbf77e0f421b2672a5199d79b94af63587ff0a37
Tarekda1 <tare2.da@gmail.com>
2020-04-28 09:28:10 -0400

    adding backend

--- client/src/context/AppReducer.js
+++ client/src/context/AppReducer.js
@@ -1,16 +1,23 @@
 export default (state, { type, payload }) => {
-    switch (type) {
-        case 'DELETE_TRANSACTION':
-            return {
-                ...state,
-                transactions: state.transactions.filter(transaction => transaction.id !== payload)
-            }
-        case 'ADD_TRANSACTION':
-            return {
-                ...state,
-                transactions: [payload, ...state.transactions]
-            }
-        default:
-            return state;
-    }
-}
+  switch (type) {
+    case "DELETE_TRANSACTION":
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          transaction => transaction.id !== payload
+        )
+      };
+    case "ADD_TRANSACTION":
+      return {
+        ...state,
+        transactions: [payload, ...state.transactions]
+      };
+    case "GET_TRANSACTIONS":
+      return {
+        ...state,
+        transactions: [...payload]
+      };
+    default:
+      return state;
+  }
+};
--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -1,15 +1,18 @@
-import React, { createContext, useReducer } from "react"
-import AppReducer from './AppReducer'
+import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+import { axios } from "axios";
 
 //Initial State
 const initialState = {
-    transactions: [
-        { id: 1, text: 'Flower', amount: -20 },
-        { id: 2, text: 'Salary', amount: 300 },
-        { id: 3, text: 'Book', amount: -10 },
-        { id: 4, text: 'Camera', amount: 150 }
-    ]
-}
+  transactions: [],
+  error: null,
+  loading: true
+};
+
+// { id: 1, text: 'Flower', amount: -20 },
+// { id: 2, text: 'Salary', amount: 300 },
+// { id: 3, text: 'Book', amount: -10 },
+// { id: 4, text: 'Camera', amount: 150 }
 
 // Create Context
 export const GlobalContext = createContext(initialState);
@@ -17,25 +20,42 @@ export const GlobalContext = createContext(initialState);
 // Provider component
 
 export const GlobalProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(AppReducer, initialState);
+  const [state, dispatch] = useReducer(AppReducer, initialState);
 
-    //Actions
-    function deleteTransaction(id) {
-        dispatch({
-            type: 'DELETE_TRANSACTION',
-            payload: id
-        });
-    }
+  //Actions
+  async function getTransactions() {
+    try {
+      const res = await axios.get("/api/v1/transactions");
+      dispatch({
+        type: "GET_TRANSACTIONS",
+        payload: res.data.data
+      });
+    } catch (error) {}
+  }
 
-    function addTransaction(transaction) {
-        dispatch({
-            type: 'ADD_TRANSACTION',
-            payload: transaction
-        });
-    }
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id
+    });
+  }
 
-    return (<GlobalContext.Provider value={{ transactions: state.transactions, deleteTransaction, addTransaction }}>
-        {children}
-    </GlobalContext.Provider>);
-}
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction
+    });
+  }
 
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
